235565bda9070a9e8edcd1386ad2661cb1481b6a
galderic <galderic@gmail.com>
2021-02-22 00:42:09 +0100

    removed hard coded values

--- src/test/scala/org/gp/scratch/util/ImageGeneratorTest.scala
+++ src/test/scala/org/gp/scratch/util/ImageGeneratorTest.scala
@@ -8,8 +8,8 @@ import org.scalatest.matchers.should
 class ImageGeneratorTest extends AnyFlatSpec with should.Matchers {
 
   "ImageGenerator" should "generate image from one of the labels" in {
-    val x = new MNISTDataSet(true)
-    val batch = x.getBatchIterator(100).next()
-    ImageGenerator.fromGray(batch, 1, 28, 28, "test")
+    val mnist = new MNISTDataSet(true)
+    val batch = mnist.getBatchIterator(1).next()
+    ImageGenerator.fromGray(batch, 0, mnist.height, mnist.width, "test")
   }
 }
