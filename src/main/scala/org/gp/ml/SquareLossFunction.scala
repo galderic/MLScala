0c94a977c6f598353e4684ba3957dda556430b1e
galderic <galderic@gmail.com>
2021-05-03 01:28:33 +0200

    fixed loss function derivative

--- src/main/scala/org/gp/ml/SquareLossFunction.scala
+++ src/main/scala/org/gp/ml/SquareLossFunction.scala
@@ -3,7 +3,7 @@ package org.gp.ml
 import org.nd4j.linalg.api.ndarray.INDArray
 
 class SquareLossFunction extends LossFunction {
-  def loss(y: INDArray, y_pred: INDArray) = y.squaredDistance(y_pred)
+  def loss(y: INDArray, y_pred: INDArray): Double = y.squaredDistance(y_pred)
 
-  def gradient(y: INDArray, y_pred: INDArray): INDArray = y.sub(y_pred).mul(-1).div(2)
+  def gradient(y: INDArray, y_pred: INDArray): INDArray = y.sub(y_pred).mul(-2)
 }
